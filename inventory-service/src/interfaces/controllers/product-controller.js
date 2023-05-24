abfabcf11300e732232620fa8168b9e6b47fbb55
Farhan Arifandi <farfnd@yahoo.com>
2023-05-24 22:27:54 +0800

    fix use case method

--- inventory-service/src/interfaces/controllers/product-controller.js
+++ inventory-service/src/interfaces/controllers/product-controller.js
@@ -2,7 +2,7 @@ module.exports = (usecase) => {
     const controller = {
         index: async (_, res) => {
             try {
-                const data = await usecase.getAlls()
+                const data = await usecase.getAll()
                 res.send(data)
             } catch (error) {
                 res.statusCode = 500
