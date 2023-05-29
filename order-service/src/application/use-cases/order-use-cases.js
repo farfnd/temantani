42749c74ed9dc9b63fb5594f0282dd92ccb7e0de
Farhan Arifandi <farfnd@yahoo.com>
2023-05-30 02:25:34 +0800

    oder: change update payment status

--- order-service/src/application/use-cases/order-use-cases.js
+++ order-service/src/application/use-cases/order-use-cases.js
@@ -1,30 +1,33 @@
 const AbstractUseCase = require('./abstracts/base-use-cases');
 const OrderStatus = require('../../domain/enums/OrderStatus');
+const errors = require('../../support/errors');
 
 class OrderUseCase extends AbstractUseCase {
     constructor(repository) {
         super(repository);
     }
-    
-    async updatePaymentStatus(orderId, transactionId, statusCode, grossAmount, paymentType) {
-        const statusCodeMapping = {
-            '200': OrderStatus.PAID,
-            '201': OrderStatus.PENDING,
-        };
 
-        const orderStatus = statusCodeMapping[statusCode] || OrderStatus.CANCELLED;
+    async paymentPaid(orderId, transactionId, grossAmount, paymentType) {
+        const order = await this.repository.getById(orderId);
+        if (!order) throw errors.NotFound('Order payment is expired');
+
+        const currentTime = moment();
+        const expiryTime = moment(order.createdAt).add(config.midtrans.expiry.duration, config.midtrans.expiry.unit);
+
+        if (currentTime.isAfter(expiryTime)) {
+            throw errors.BadRequest('Order payment is expired');
+        }
 
         try {
-            // Update the order status in the database
             await this.repository.update(orderId, {
                 transactionId,
-                orderStatus,
+                orderStatus: OrderStatus.PAID,
                 paymentMethod: paymentType,
                 paymentAmount: grossAmount
             });
         } catch (error) {
             console.log(error);
-            throw new Error('Failed to update order status');
+            throw errors.InternalServerError('Error updating order status');
         }
     }
 }
